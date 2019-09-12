a4fd6d1c729dae3164a6a02d8714a68e67898ec1
Bruno Amaral <bruno_henrique_a@hotmail.com>
2019-09-12 19:06:28 -0300

    Exemplo simples do uso de Linq

---
+++ Linq/Program.cs
@@ -0,0 +1,29 @@
+﻿ using System;
+using System.Linq;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Especificando DataSouce
+
+            int[] numbers = new int[] { 2, 3, 4, 5 };
+
+            //Definir a consulta
+
+            var result = numbers
+                .Where(x => x % 2 == 0)
+                .Select(x => x * 10);
+
+            //Executar a consulta
+
+            foreach (int x in result)
+            {
+                Console.WriteLine(x);
+            }
+
+        }
+    }
+}
